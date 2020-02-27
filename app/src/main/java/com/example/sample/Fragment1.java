272d1156a7c0300336b6a71abecc747e56d6daf7
jiwook2000 <56825960+jiwook2000@users.noreply.github.com>
2020-02-27 19:31:04 +0900

    Homework-broken

--- app/src/main/java/com/example/sample/Fragment1.java
+++ app/src/main/java/com/example/sample/Fragment1.java
@@ -1,6 +1,54 @@
 package com.example.sample;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import javax.xml.datatype.Duration;
+
 public class Fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1,container,false);
+
+        final EditText name = rootView.findViewById(R.id.name);
+        final EditText age = rootView.findViewById(R.id.age);
+
+
+        Button btn = rootView.findViewById(R.id.btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),name.getText().toString()+"님, "+age.getText().toString()+"세,  회원 등록되었습니다.",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        return rootView;
+    }
+
+
+
 }
